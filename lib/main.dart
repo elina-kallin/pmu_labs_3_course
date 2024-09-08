567266d236bfe961702688c5ec289d480bdf6955
Елена Бакальская <elina_kallin@mail.ru>
2024-09-08 20:43:13 +0400

    first lab is done

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Text( 
+              'Жми меня скок хочешь',
             ),
             Text(
               '$_counter',
