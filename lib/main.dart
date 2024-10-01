838f5f050266b27bd9897fcfbc476b7559a9d77b
Елена Бакальская <elina_kallin@mail.ru>
2024-10-01 19:03:12 +0400

    lab3 is ready

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Бакальская Елена'),
+    return const MaterialApp(
+      title: 'Безымянная соцсеть',
+      home: MyHomePage(title: 'Бакальская Елена'),
     );
   }
 }
@@ -30,40 +26,125 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final Color _color = Colors.teal;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget. title),
+        backgroundColor: _color,
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text( 
-              'Жми меня скок хочешь',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: const MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = [
+      _CardPostData("Первая из списка карточек ыть", imageUrl: "./assets/images/popug.jpg"),
+      _CardPostData("Вторая из списка карточек ыть", imageUrl: "./assets/images/popug.jpg"),
+      _CardPostData("Третья из списка каточек ыть", imageUrl: "./assets/images/popug.jpg"),
+    ];
+    return Center(
+        child: SingleChildScrollView(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: data.map((e) => _CardPost.fromData(e)).toList(),
+        //           _CardPost('jfbjdf'),
+        //           _CardPost('fdg'),
+        //           _CardPost('sfgsf'),
+
+    )));
+  }
+}
+
+class _CardPost extends StatelessWidget {
+  final String description;
+  final String? imageUrl;
+
+  const _CardPost(this.description, {this.imageUrl});
+
+  factory _CardPost.fromData(_CardPostData data) =>
+      _CardPost(data.description, imageUrl: data.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.teal,
+        border: Border.all(
+          color: const Color.fromARGB(255, 64, 46, 9),
+          width: 0.5,
         ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(
+              0.0,
+              5.0,
+            ),
+            blurRadius: 4.0,
+            spreadRadius: 1.0,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.ac_unit_outlined),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Text(
+                  description,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // IMAGE ROW
+          const SizedBox(height: 10),
+          if (imageUrl != null)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(imageUrl!, height: 150, width: double.infinity, fit: BoxFit.cover),
+            ),
+
+          const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.favorite, color: Colors.pink),
+                Icon(Icons.comment, color: Colors.white),
+                Icon(Icons.share, color: Color.fromARGB(255, 15, 11, 90)),
+              ],
+            ),
+          )
+        ],
       ),
     );
+  }
+}
+
+class _CardPostData extends StatelessWidget {
+  final String description;
+  final String? imageUrl;
+
+  const _CardPostData(this.description, {this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
